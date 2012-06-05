45ec83dda3113bfd3079a0adaf5c7d39dedb0f02
Nick Bolton <nick@synergy-project.org>
2012-06-05 20:25:25 +0000

    skeleton for support plugin.

---
+++ plugins/Support/SupportController.class.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * SPIT: Simple PHP Issue Tracker
+ * Copyright (C) 2012 Nick Bolton
+ * 
+ * This package is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * found in the file COPYING that should have accompanied this file.
+ * 
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class SupportController extends Spit\Controllers\Controller {
+  
+  public function __construct() {
+    $this->viewDir = "php/plugins/Support/views/";
+  }
+  
+  public function run() {
+    $this->showView("support");
+  }
+}
+
+?>
