95f8e8365162f1f4adccf5277240e152b0fbde2a
Nick Bolton <nick@synergy-project.org>
2014-07-14 10:00:28 +0000

    fixed includes

--- plugins/Pages/Pages.php
+++ plugins/Pages/Pages.php
@@ -17,8 +17,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-require "DownloadController.class.php";
-require "ContactController.class.php";
+require "DownloadController.php";
+require "ContactController.php";
 
 class Pages {
   
